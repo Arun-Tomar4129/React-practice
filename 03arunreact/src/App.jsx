4945089124d5f0d28a0dadd71065f3bff84d61d3
Arun-Tomar4129 <arunrajput412995@gmail.com>
2025-04-14 08:00:24 +0530

    create small project

---
+++ 03arunreact/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+
+
+function Example() {
+  return (
+    <>
+      <h1 style={{ backgroundColor: 'lightblue', color: 'black', padding: '10px' }}>Arun Tomar</h1>
+      {/* <h2 style={{ backgroundColor: 'yellow', fontWeight: 'bold', margin: '5px 0',border: 40px solid brlue, }}>arun tomarc</h2> */}
+      <h2 style={{ backgroundColor: 'grey', fontWeight: 'bold', margin: '5px 0', border: '40px solid blue' }}>Work on React</h2>
+    </>
+  )
+}
+
+export default Example
