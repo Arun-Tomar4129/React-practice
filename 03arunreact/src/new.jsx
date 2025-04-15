421e8a6d17c3e8d7267a860294e496c232090bbb
Arun-Tomar4129 <arunrajput412995@gmail.com>
2025-04-15 20:26:24 +0530

    make new project

---
+++ 03arunreact/src/new.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid';
+
+function App(props) { // Renamed 'pro' to 'props' for convention
+  return (
+    <h1>new file</h1>
+
+
+
+  );
+  
+}
+export default App;
