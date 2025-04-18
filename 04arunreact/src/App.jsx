e79a77e4e9b9ffae24d13b0f57d58dbcb0ce416f
Arun-Tomar4129 <arunrajput412995@gmail.com>
2025-04-18 21:20:54 +0530

    make color change project

--- 04arunreact/src/App.jsx
+++ 04arunreact/src/App.jsx
@@ -4,30 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("black"); // Example default color
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-screen duration-200"
+      style={{backgroundColor: color,width: "100vw", height: "100vh" }}>
+        <div className="fixed flex  inset-x-0 px-2 "> 
+          <div className='flex flex-wrap jusify-center gap-4 shadow-lg bg-white px-2 py-3 ' style={{width:"800px", background:"yellow", height:"80px" ,border:"20px"}}>
+            <button onClick={()=> setColor("red")} className='outline-none px-4 py-1 rounded-full  text-white'>red</button>
+            <button onClick={()=> setColor("blue")} className='outline-none px-4 py-1 rounded-full  text-white'>blue</button>
+            <button onClick={()=> setColor("green")} className='outline-none px-4 py-1 rounded-full  text-white'>green</button>
+            <button onClick={()=> setColor("pink")} className='outline-none px-4 py-1 rounded-full  text-white'>pink</button>
+            <button onClick={()=> setColor("orange")} className='outline-none px-4 py-1 rounded-full  text-white'>orange</button>
+            <button onClick={()=> setColor("brown")} className='outline-none px-4 py-1 rounded-full  text-white'>brown</button>
+            <button onClick={()=> setColor("darkblue")} className='outline-none px-4 py-1 rounded-full  text-white'>darkblue</button>
+            <button onClick={()=> setColor("skyblue")} className='outline-none px-4 py-1 rounded-full  text-white'>skyblue</button>
+          </div>
+        </div>
+
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
