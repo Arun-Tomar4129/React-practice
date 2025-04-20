b21ad9d15078806638d26802f2eb616f505f4ff2
Arun-Tomar4129 <arunrajput412995@gmail.com>
2025-04-20 23:16:51 +0530

    mistake add git icon

--- 05arunreact/src/App.jsx
+++ 05arunreact/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useCallback } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
-import "./App.css";
+import "tailwindcss";
+
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
@@ -25,9 +25,29 @@ function App() {
   }, [length, numberAllowed, charAllowed]);
 
   return (
-    <><div style={{ backgroundColor: "gray", padding: "10px" }}>
-    {password ? password : "Generate a password"}
-  </div>
+    <>
+      <div className="w-full max-w-md mx-auto shadow rounded-lg px-4 my-8 text-orange-500 bg-gray-700">
+        <h1 className="text-white text-center my-3">password generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4 py-3 ">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3 bf bg-white"
+            placeholder="password"
+            readOnly
+          />
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            {" "}
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-1 ">
+          <div className="flex text-sm gap-x-1 ">
+            <input type="range" min={6} max={100} value={length} className="cursor-pointer" onChange={(e)=>{setLength{e.target.value}}} />
+            <label > Length:{length}</label>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
